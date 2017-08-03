bc5532dba1249a6a411632ea7e2658881925eaa9
郏要 <jiayaoit@126.com>
2017-08-03 11:00:27 -0700

    Initial Commit

---
+++ JYCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYCategory
+//
+//  Created by Dely on 2017/8/3.
+//  Copyright © 2017年 Dely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYCategory/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JYCategory
+//
+//  Created by Dely on 2017/8/3.
+//  Copyright © 2017年 Dely. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
